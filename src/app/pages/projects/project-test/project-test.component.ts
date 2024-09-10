84a628f0bc1d2cc00b81850214a0906fd018d66c
joelsub <joelsubiabre01@gmail.com>
2024-09-09 23:15:12 -0300

    cambios interfaz principal

--- src/app/pages/projects/project-test/project-test.component.ts
+++ src/app/pages/projects/project-test/project-test.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList, ElementRef  } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 
 @Component({
   selector: 'app-project-test',
   templateUrl: './project-test.component.html',
   styleUrl: './project-test.component.scss'
 })
-export class ProjectTestComponent {
-  constructor() { }
+export class ProjectTestComponent implements OnInit{
+
+  constructor() {
+  
+  }
 
   ngOnInit(): void {
+
   }
 }
